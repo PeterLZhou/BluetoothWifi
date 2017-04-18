a252dda1fff6e2d53c24111206036a8cd09dae0d
PeterLZhou <peter.zhou@yale.edu>
2017-04-18 17:18:58 -0400

    Set separate activities for BT and Wifi

--- app/src/main/java/com/peterlzhou/bluetoothwifi/MainActivity.java
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.peterlzhou.bluetoothwifi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button wifiButton = (Button) findViewById(R.id.wifi);
+        Button bluetoothButton = (Button) findViewById(R.id.bluetooth);
+
+        //Starts the wifi listening/sending section
+        wifiButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        startWifi(v);
+                    }
+                }
+        );
+
+        //Starts the bluetooth listening/sending section
+        bluetoothButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        startBluetooth(v);
+                    }
+                }
+        );
+    }
+
+    public void startWifi(View view){
+        Intent intent = new Intent(this, WifiActivity.class);
+        startActivity(intent);
+    }
+
+    public void startBluetooth(View view){
+        Intent intent = new Intent(this, BluetoothActivity.class);
+        startActivity(intent);
     }
 }
+
---
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/WifiActivity.java
@@ -0,0 +1,16 @@
+package com.peterlzhou.bluetoothwifi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by peterlzhou on 4/18/17.
+ */
+
+public class WifiActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifilayout);
+    }
+}
