dc3a4995b7f13634f1b4fc7cb943ff42afd1b20d
PeterLZhou <peter.zhou@yale.edu>
2017-04-18 18:36:51 -0400

    Wifi Broadcasting works

---
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/Receiver.java
@@ -0,0 +1,52 @@
+package com.peterlzhou.bluetoothwifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+
+/**
+ * Created by peterlzhou on 4/18/17.
+ */
+
+public class Receiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private WifiActivity mActivity;
+
+    public Receiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       WifiActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                System.out.println("Wifi is on");
+            } else {
+                System.out.println("Wifi is off");
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // The peer list has changed!  We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed!  We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            //Device's wifi state changed
+        }
+    }
+
+}
--- app/src/main/java/com/peterlzhou/bluetoothwifi/WifiActivity.java
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/WifiActivity.java
@@ -1,5 +1,9 @@
 package com.peterlzhou.bluetoothwifi;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,9 +12,46 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class WifiActivity extends AppCompatActivity {
+
+    private final IntentFilter mIntentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
+    BroadcastReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        System.out.println("Wifi Activity started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wifilayout);
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new Receiver(mManager, mChannel, this);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                System.out.println("Found em'!");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                System.out.println("Error code" + reasonCode);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
     }
 }
