b7e9f21a04569835f61373f3a8a4086183f465db
rtung1333 <gnuttrebor07@gmail.com>
2017-04-20 00:48:52 -0400

    Added some more code to connect to Bluetooth LE but not debugged as second device does not have Bluetooth LE

--- app/src/main/java/com/peterlzhou/bluetoothwifi/BluetoothActivity.java
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/BluetoothActivity.java
@@ -3,6 +3,23 @@ package com.peterlzhou.bluetoothwifi;
 /*
  * Note: This class was largely taken from from https://github.com/googlesamples/android-BluetoothLeGatt
  * strings.xml was also adapted from this sample code.
+ * Adapted from the code from the copyright below to fit our application:
+ * We have left in the copyright message to make this clear
+ */
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import android.app.Activity;
--- app/src/main/java/com/peterlzhou/bluetoothwifi/BluetoothSendMessageActivity.java
+++ app/src/main/java/com/peterlzhou/bluetoothwifi/BluetoothSendMessageActivity.java
@@ -1,8 +1,11 @@
 package com.peterlzhou.bluetoothwifi;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,7 @@ import android.widget.TextView;
  */
 
 public class BluetoothSendMessageActivity extends Activity {
+    private final static String TAG = BluetoothSendMessageActivity.class.getSimpleName();
 
     public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
@@ -21,8 +25,31 @@ public class BluetoothSendMessageActivity extends Activity {
     private String mDeviceName;
     private String mDeviceAddress;
 
+    private BluetoothLeService mBluetoothLeService;
+
     Button mButton;
     EditText mEdit;
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            Log.v(TAG, "On service connected");
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(BluetoothSendMessageActivity.class.getSimpleName(), "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
 
     /** Called when the activity is first created. */
     @Override
@@ -47,27 +74,9 @@ public class BluetoothSendMessageActivity extends Activity {
         mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
         mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
         ((TextView) findViewById(R.id.bt_device_name)).setText(mDeviceName);
-    }
 
-    /*@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // setContentView(R.layout.gatt_services_characteristics);
-
-        final Intent intent = getIntent();
-        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
-        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
-
-        // Sets up UI references.
-        ((TextView) findViewById(R.id.device_address)).setText(mDeviceAddress);
-        /*mGattServicesList = (ExpandableListView) findViewById(R.id.gatt_services_list);
-        mGattServicesList.setOnChildClickListener(servicesListClickListner);
-        mConnectionState = (TextView) findViewById(R.id.connection_state);
-        mDataField = (TextView) findViewById(R.id.data_value);
-
-        getActionBar().setTitle(mDeviceName);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        /*Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
-    }*/
+        Log.v(TAG, "Bound service");
+    }
 }
